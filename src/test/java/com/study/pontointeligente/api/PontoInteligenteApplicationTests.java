4d47b9e4755cda1eefd0ae37bddd134fe0d59b8e
Max Azevêdo <max.azvd@gmail.com>
2020-04-17 18:44:17 -0300

    Commit inicial

---
+++ src/test/java/com/study/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
